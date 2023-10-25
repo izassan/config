7e61ffbd34132a1cbc9088fa197d5e8ded41b469
izassan <izassan.si@gmail.com>
2023-10-25 13:41:16 +0000

    remove: remove symbolic link to gitconfig

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,2 @@
 New-Item -ItemType SymbolicLink -Path $HOME\Documents -Name Powershell -Value $PWD\powershell
 New-Item -ItemType SymbolicLink -Path $HOME -Name vimfiles -Value $PWD\vim
-New-Item -ItemType SymbolicLink -Path $HOME -Name .gitconfig -Value $PWD\.gitconfig
