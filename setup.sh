a40cadb4402b22ee6a732d6f7a631167ef699369
izassan <izassan.si@gmail.com>
2023-10-25 13:40:47 +0000

    update: replace git config files.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 ln -sfnv $PWD/vim $HOME/.vim
+
 ln -sfnv $PWD/zsh $HOME/.zsh
 ln -sfnv $PWD/zsh/zshrc $HOME/.zsh/.zshrc
 ln -sfnv $PWD/zshenv $HOME/.zshenv
 
+mkdir -p $HOME/.config
+ln -sfnv $PWD/git $HOME/.config/git
