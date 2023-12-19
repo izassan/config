67bae33c78de03e8a6d8e5ed377170dd227a9cff
izassan <izassan.si@gmail.com>
2023-12-19 23:12:15 +0900

    update: added creating git symboliclink

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,5 @@
 New-Item -ItemType SymbolicLink -Path $HOME\Documents -Name Powershell -Value $PWD\powershell
 New-Item -ItemType SymbolicLink -Path $HOME -Name vimfiles -Value $PWD\vim
+
+New-Item -ItemType Directory -Path $HOME -Name .config
+New-Item -ItemType SymbolicLink -Path $HOME\.config -Name git -Value $PWD\git
