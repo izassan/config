e424f1ca633095d58febbcb8095a6aa6a235a539
izassan <izassan.si@gmail.com>
2024-03-24 13:17:48 +0900

    update: change aliases. added functions and aliases

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,26 +1,21 @@
-# functions
-function alias ($alias_name, $cmd){
-    if(Get-Alias $alias_name 2> $null){ Remove-Alias -Force $alias_name }
-    $null = New-Item -Path function: -Name "script:$alias_name" -Value "$cmd `$args"
-}
-
-function b64 ([switch]$Decode, [string]$TargetValue){
-    if($Decode){ # base64 decoding
-        $result = [System.Text.Encoding]::Default.GetString(
-            [System.Convert]::FromBase64String($TargetValue))
-    }else{ # base64 encoding
-        $result = [Convert]::ToBase64String(
-            ([System.Text.Encoding]::Default).GetBytes($TargetValue))
-    }
-    Write-Output $result
-}
-
-function command_exists($cmd){
+function is_exist_command($cmd){
     Get-Command $cmd -ea SilentlyContinue | Out-Null; return $?
 }
 
+function b64 ([string]$plainText){
+    $b64Str = [Convert]::ToBase64String(
+        ([System.Text.Encoding]::Default).GetBytes($plainText))
+    return $b64Str
+}
+
+function b64d ([string]$b64Str){
+    $plainText = [System.Text.Encoding]::Default.GetString(
+        [System.Convert]::FromBase64String($b64Str))
+    return $plainText
+}
+
 function change_repository(){
-    if(!((command_exists "ghq") -And (command_exists "fzf"))){ return }
+    if(!((is_exist_command "ghq") -And (is_exist_command "fzf"))){ return }
     Set-Location (ghq list --full-path | fzf)
 }
 
@@ -38,55 +33,82 @@ function config_init(){
     }
 }
 
-function ssh_copy_id(){
-    param($dest)
+function copy_ssh_id_to_remote_machine(){
+    Param($dest)
     $cmd = "mkdir -p ~/.ssh && chmod 700 ~/.ssh &&"
     $cmd = "$cmd cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
     Get-Content ~/.ssh/id_rsa.pub | ssh $dest "$cmd"
 }
 
+function remove_multi_files(){
+    Param(
+        [array]$Paths,
+        [switch]$Recurse,
+        [switch]$Force
+    )
+    foreach($p in $Paths){
+        if(($Recurse -And $Force)){
+            Remove-Item $p -Force -Recurse
+        }elseif($Recurse){
+            Remove-Item $p -Recurse
+        }elseif($Force){
+            Remove-Item $p -Force
+        }else{
+            Remove-Item $p
+        }
+    }
+}
+
 # environment variables
+$env:GOPATH = "$HOME\.go"
 $env:PATH = "$env:PATH;$env:GOPATH\bin"
 
-
 # aliases
-alias "cr" "change_repository"
-alias "op" "explorer.exe"
-alias "opc" "explorer.exe ."
-alias "br" "Start-Process chrome"
+function ls(){ Get-ChildItem -Exclude _*,.* $Args }
+function lsn(){ Get-ChildItem -Name -Exclude _*,.* $Args }
+function lsf(){ Get-ChildItem -Name -Force $Args }
+function cr(){ change_repository $Args }
+function op(){ explorer.exe $Args }
+function opc(){ explorer.exe . $Args }
+function br(){ Start-Process chrome $Args }
+function cfg(){ Set-Location $HOME/Config $Args }
+function ssh-copy-id(){ copy_ssh_id_to_remote_machine $Args }
+function mrm(){ remove_multi_files $Args }
+function mrmr(){ remove_multi_files -Recurse $Args }
+function mrmrf(){ remove_multi_files -Recurse -Force $Args }
+function rmr(){ Remove-Item -Recurse $Args }
+function rmrf(){ Remove-Item -Recurse -Force $Args }
+function exp(){ Expand-Archive $Args }
+function ec(){ Write-Output $Args }
+function cpr(){ Copy-Item -Recurse $Args }
+function touch(){ Out-File -InputObject "" -FilePath "$Args" }
 
-alias "gst" "git status"
-alias "ga" "git add"
-alias "gap" "git add -p"
-alias "gc" "git commit"
-alias "gcm" "git commit -m"
-alias "gd" "git diff"
-alias "gdc" "git diff --cached"
-alias "gs" "git switch"
-alias "gsc" "git switch -c"
+function gst(){ git status $Args }
+function ga(){ git add $Args }
+function gap(){ git add -p $Args }
+function gc(){ git commit $Args }
+function gcm(){ git commit -m $Args }
+function gd(){ git diff $Args }
+function gdc(){ git diff --cached $Args }
+function gs(){ git switch $Args }
+function gsc(){ git switch -c $Args }
+function gb(){ git branch $Args }
+function gba(){ git branch -a $Args }
+function gbd(){ git branch -d $Args }
 
-alias "mp" "multipass"
-alias "mpl" "multipass list"
-alias "mpu" "multipass start"
-alias "mpd" "multipass stop"
-alias "mps" "multipass shell"
-alias "mpc" "multipass shell dev-vm"
+function mp(){ multipass $Args }
+function mpl(){ multipass list $Args }
+function mpu(){ multipass start $Args }
+function mpd(){ multipass stop $Args }
+function mps(){ multipass shell $Args }
+function mpc(){ multipass shell dev-vm $Args }
 
-alias "cfg" "Set-Location $HOME/Config"
-alias "ssh-copy-id" "ssh_copy_id"
+function wg(){ winget $Args }
+function wgi(){ winget install $Args }
+function wgl(){ winget list $Args }
 
-alias "wg" "winget"
-alias "wgi" "winget install"
-alias "wgl" "winget list"
-
-alias "lsn" "Get-ChildItem -Name"
-alias "rmr" "Remove-Item -Recurse"
-alias "rmrf" "Remove-Item -Recurse -Force"
-alias "exp" "Expand-Archive"
-alias "ec" "Write-Output"
-alias "mv" "Move-Item"
-alias "cp" "Copy-Item"
-alias "cpr" "Copy-Item -Recurse"
+# unset conflicted aliases
+Remove-Alias ls
 
 # key bindings
 Import-Module PSReadline
