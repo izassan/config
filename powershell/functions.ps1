7a10a08e2d243880952d0828ba2eaf6405f60136
izassan <izassan.si@gmail.com>
2024-07-03 21:56:12 +0900

    update: added functions extracting zip and compress zip

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -12,3 +12,49 @@ function ConvertFrom-Base64String([string]$b64Str){
         [System.Convert]::FromBase64String($b64Str))
     return $plainText
 }
+
+function Expand-ZipArchive(){
+    Param(
+        [string][Parameter(Mandatory)]$zipFilePath,
+        [string]$extractToDirectory = ""
+    )
+    if(-Not(Test-Path $zipFilePath)){
+        Write-Error "not found $zipFilePath"
+        return
+    }
+    $p = Convert-Path $zipFilePath
+    if($extractToDirectory -eq ""){
+        $extractToDirectory = Join-Path $PWD (Split-Path $zipFilePath -LeafBase)
+    }elseif(-Not(Split-Path -IsAbsolute $extractToDirectory)){
+        $extractToDirectory = Join-Path $PWD (Split-Path $extractToDirectory -LeafBase)
+    }
+    $e = [Uri]::new($extractToDirectory).LocalPath
+    Write-Host "Extracting..."
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($p, $e)
+    Write-Host "Extracted to $extractToDirectory"
+}
+
+function Compress-ZipArchive(){
+    Param(
+        [string][Parameter(Mandatory)]$compressDirectory,
+        [string]$zipFilePath = ""
+    )
+    if(-Not(Test-Path $compressDirectory)){
+        Write-Error "not found $compressDirectory"
+        return
+    }
+    $c = Convert-Path $compressDirectory
+    if($zipFilePath -eq ""){
+        $zipFilePath = (Join-Path $PWD (Split-Path $compressDirectory -Leaf)) + ".zip"
+    }elseif(-Not(Split-Path -IsAbsolute $zipFilePath)){
+        if((Split-Path $zipFilePath -Extension) -eq ".zip"){
+            $zipFilePath = (Join-Path $PWD (Split-Path $zipFilePath -Leaf))
+        }else{
+            $zipFilePath = (Join-Path $PWD (Split-Path $zipFilePath -Leaf)) + ".zip"
+        }
+    }
+    $z = [Uri]::new($zipFilePath).LocalPath
+    Write-Host "Compressing..."
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($c, $z)
+    Write-Host "compressed to $zipFilePath"
+}
