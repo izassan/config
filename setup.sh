81bfab9fbd945457967a121c9faef2aeb88ea760
izassan <izassan.si@gmail.com>
2024-08-25 03:33:47 +0900

    update: added configuration files of desktop environment

--- setup.sh
+++ setup.sh
@@ -9,3 +9,10 @@ ln -sfnv $PWD/tmux.conf $HOME/.tmux.conf
 ln -sfnv $PWD/zsh $HOME/.zsh
 ln -sfnv $PWD/zsh/zshrc $HOME/.zshrc
 ln -sfnv $PWD/zsh/zshenv $HOME/.zshenv
+
+mkdir -p $HOME/.config
+ln -sfnv $PWD/picom.conf $HOME/.config/picom.conf
+ln -sfnv $PWD/i3 $HOME/.config/i3
+ln -sfnv $PWD/rofi $HOME/.config/rofi
+ln -sfnv $PWD/conky $HOME/.config/conky
+ln -sfnv $PWD/terminator $HOME/.config/terminator
