f14fbe115f5c6c55e58858a0e41fea34545f6c05
izassan <izassan.si@gmail.com>
2024-09-08 00:10:16 +0900

    update: update rofi configuration

--- rofi/rofi_system.sh
+++ rofi/rofi_system.sh
@@ -3,15 +3,27 @@
 set -euCo pipefail
 
 function main() {
-  local -Ar menu=(
-    ['Lock']='dm-tool lock'
-    ['Logout']='i3-msg exit'
-    ['Poweroff']='systemctl poweroff'
-    ['Reboot']='systemctl reboot'
-  )
+    local -Ar menu=(
+        ['Lock']='dm-tool lock'
+        ['Logout']='i3-msg exit'
+        ['Poweroff']='systemctl poweroff'
+        ['Reboot']='systemctl reboot'
+    )
 
-  local -r IFS=$'\n'
-  [[ $# -ne 0 ]] && eval "${menu[$1]}" || echo "${!menu[*]}"
+    local -r IFS=$'\n'
+    local -r yes='yes' no='no'
+    if [[ $# -eq 0 ]];then
+        echo "${!menu[*]}"
+    elif [[ $# -eq 1 ]];then
+        echo $1 ${yes} 
+        echo $1 ${no} 
+    else
+        if [[ $2 == ${yes} ]];then
+            eval "${menu[$1]}"
+        else
+            echo "${!menu[*]}"
+        fi
+    fi
 }
 
 main $@
