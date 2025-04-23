bfe3bfa1ceb92dcc435df8dbfa70de31a54df755
izassan <izassan.si@gmail.com>
2025-04-24 07:23:04 +0900

    update: update zsh configuration
    Changes:
    - enable viewing main configuration
    - update aliases
    
    Reason:

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,16 +1,7 @@
-alias -g G='| grep'
-alias -g E='2> /dev/null'
-alias -g Z='> /dev/null'
-
 alias l="ls --color=auto"
 alias ls="ls --color=auto"
 alias la="ls -a --color=auto"
 alias ll="ls -al --color=auto"
-
-alias ..="cd .."
-alias ..2="cd_parent 2"
-alias ..3="cd_parent 3"
-alias cdg="cd-gitroot"
 
 alias cl="clear"
 alias cfg="cd ~/Config"
@@ -19,33 +10,18 @@ alias cp="cp -i"
 alias mv="mv -i"
 alias rm="rm -i"
 
-alias v="vim ."
-
 alias gst="git status"
 alias gl="git log"
-alias glo="git log --oneline"
+alias ga="git add"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gr="git reset"
 alias gd="git diff"
 alias gdc="git diff --cached"
-alias ga="git add"
-alias gap="git add -p"
-alias gc="git commit"
-alias gcm="git commit -m"
-alias gca="git commit --amend"
-alias gb="git branch"
-alias gba="git branch -a"
-alias gbd="git branch -d"
 alias gs="git switch"
 alias gsc="git switch -c"
+alias gb="git branch"
 alias gm="git merge"
-alias gr="git reset"
-alias gp="git push"
-alias gpo="git push origin"
-alias gpu="git pull"
-alias gf="git fetch"
-alias gcl="git clone"
+alias gp="git pull"
 
-alias kb="kubectl"
-alias kbg="kubectl get"
-alias kba="kubectl apply"
-
-alias tf="terraform"
+alias cdg="cd-gitroot"
---
+++ zsh/config.zsh
@@ -0,0 +1,44 @@
+source $XDG_CONFIG_HOME/zsh/functions.zsh
+source $XDG_CONFIG_HOME/zsh/prompt.zsh
+source $XDG_CONFIG_HOME/zsh/aliases.zsh
+
+if [ -d $ZPLUG_HOME ];then
+    source $ZPLUG_HOME/init.zsh
+
+    # plugins
+    zplug "zsh-users/zsh-syntax-highlighting", defer:2
+    zplug "zsh-users/zsh-autosuggestions"
+    zplug "mollifier/cd-gitroot"
+
+    # Then, source plugins and add commands to $PATH
+    zplug load
+fi
+
+bindkey -v
+
+zle -N change_repository change_repository
+bindkey '^g' change_repository
+
+autoload -U colors; colors
+autoload -Uz add-zsh-hook
+
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt correct
+setopt prompt_subst
+setopt appendhistory
+setopt share_history
+setopt hist_ignore_all_dups
+setopt inc_append_history
+setopt extended_history
+
+function set_prompt(){
+    PROMPT="%n %d > "
+    RPROMPT="`set_right_prompt`"
+}
+add-zsh-hook precmd set_prompt
+
+if [ -r $ZSH_LOCAL_CONFIG ];then
+    source $ZSH_LOCAL_CONFIG
+fi
